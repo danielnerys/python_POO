2f821b360c693e41d5fcc69d4a6ae54fee63d2fa
danielnerys <danielnerys@icloud.com>
2024-06-05 16:52:09 -0300

    encapsulamento_estudo

---
+++ encapsulamento.py
@@ -0,0 +1,15 @@
+class Conta:
+    def __init__(self, nro_agencia, saldo= 0):
+    #metodos e caracteristicas que começam com underline, por 
+        self._saldo = saldo
+        
+        self.nro_agencia = nro_agencia
+        
+    def depositar(self, valor):
+        self._saldo += valor
+        
+    def sacar(self, valor):
+        self._saldo -= valor
+        
+    def mostrar_saldo(self):
+        return self._saldo
