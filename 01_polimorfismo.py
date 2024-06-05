29ba3d9e0b422b41c9d1f56c0d4fc55210e8b8fb
danielnerys <danielnerys@icloud.com>
2024-06-05 19:21:08 -0300

    Exemplo de polimorfismo

---
+++ 01_polimorfismo.py
@@ -0,0 +1,23 @@
+class Passaro:
+    def voar(self):
+        print('Passaro pode voar')
+
+
+class Pardal:
+    def voar(self):
+        print('Pardal pode voar')
+
+
+class Avestruz:
+    def voar(self):
+        print('Avestruz não pode voar')
+
+#ambas classes Pardal e Avestruz compartilham o mesmo metodo que é voar,
+
+#no plano_voo, podemos receber o obj com o metodo, pois o metodo é polimorfo e tem valores diferentes para cada classe
+def plano_voo(obj):
+    obj.voar()
+
+
+plano_voo(Pardal())
+plano_voo(Avestruz())
